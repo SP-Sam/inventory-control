623b82e653e929572c4d3030c4bb91bbd1f483ee
Samuel Pereira <gmr.quest.10@gmail.com>
2022-05-15 19:35:18 -0300

    feat: Sets up CORS and creates the first route

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,32 @@
-import express, { Express } from 'express';
+import cors, { CorsOptions } from 'cors';
+import express, { Express, Request, Response } from 'express';
 
 class App {
   private app: Express;
 
   constructor() {
     this.app = express();
+    this.config();
+    this.routers();
+  }
+
+  private config(): void {
+    const corsOptions: CorsOptions = {
+      allowedHeaders: ['*'],
+      credentials: true,
+      methods: 'POST,GET,PUT,PATCH,DELETE,OPTIONS,HEAD',
+      origin: '*',
+      preflightContinue: false,
+    };
+
+    this.app.use(cors(corsOptions));
+    this.app.use(express.json());
+  }
+
+  private routers(): void {
+    this.app.get('/', (_req: Request, res: Response) => {
+      return res.status(200).json({ message: 'Welcome to Autoflex API!' });
+    });
   }
 
   public start(PORT: string | number): void {
